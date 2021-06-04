ce5c22d04c1ea13515c468626a5a4486a5afd3f5
Mekha789 <84712828+Mekha789@users.noreply.github.com>
2021-06-05 00:22:09 +0300

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -43,17 +43,17 @@ int main(void) {
 }
 float mc_pi(int count)
 {
-	float circle=0;
-	for(int i=0;i<count;i++)
-	{
-		
-		float x = frandom();
-		float y = frandom();
-		if(sqrt(x*x+y*y)<=1)
-		{
-			circle++;
-		}
-	}
-	float area =(float) (4*circle)/(count);
-	return area;
+ float circle=0;
+ for(int i=0;i<count;i++)
+ {
+  
+  float x = frandom();
+  float y = frandom();
+  if(sqrt(x*x+y*y)<=1)
+  {
+   circle++;
+  }
+ }
+ float area =(float) (4*circle)/(count);
+ return area;
 }
